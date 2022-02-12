0cc0a46a5526658f83b02e493e06a044ee0ca2d1
epsilon-prasad <prasad.u@epsilon.com>
2022-02-12 19:46:30 +0530

    Adding the Basic card layout

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="cardLayout">
+                    <div className="cards">
+                        <div className="cardItem">
+                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/24.png" />
+                            <div className="cardContent">
+                                <h3>arbok</h3>
+                            </div>
+                            <ul className="list-group">
+                                <li className="list-group-item">Height:35</li>
+                                <li className="list-group-item">Weight:650</li>
+                                <li className="list-group-item">
+                                    List of abilities:intimidate, shed-skin,
+                                    unnerve
+                                </li>
+                            </ul>
+                            <div className="cardContent">
+                                <button type="button" className="btn">
+                                    View Details
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default App;
