49dbfe2bdd14583545b21b453324c6994cb86cc3
epsilon-prasad <prasad.u@epsilon.com>
2022-02-12 20:11:11 +0530

    Adding the data bind

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,32 +1,112 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
+    const [dataList, setDataList] = useState([]);
+    const [pagination, setPagination] = useState({
+        limit: 30,
+        offset: 0,
+        next: null,
+        prev: null,
+    });
+    useEffect(() => {
+        setDataList([]);
+        fetchDataList(pagination.limit, pagination.offset);
+    }, [pagination.limit]);
+
+    const fetchDataList = async (urlData) => {
+        let findResults;
+        try {
+            let URL;
+            if (urlData.length) {
+                URL = urlData;
+            } else {
+                URL = `https://pokeapi.co/api/v2/pokemon?limit=${pagination.limit}&offset=${pagination.offset}`;
+            }
+
+            let response = await fetch(URL);
+            if (!response.ok) {
+                throw new Error("Something went wrong!");
+            }
+            const data = await response.json();
+            setPagination((prevState) => ({
+                ...prevState,
+                next: data.next,
+                prev: data.previous,
+            }));
+
+            const responses = await Promise.all(
+                data.results.map((item) => {
+                    return fetch(item.url);
+                })
+            );
+
+            const filteredResponses = responses.filter(
+                (res) => res.status === 200
+            );
+
+            findResults = Promise.all(
+                filteredResponses.map(async (item) => {
+                    const details = await item.json();
+                    return details;
+                })
+            );
+        } catch (err) {
+            console.log(err, "err");
+        }
+
+        findResults.then((res) => {
+            setDataList(res);
+        });
+    };
+
+    const Item = (itemData) => {
+        const { data } = itemData;
+        return (
+            <div className="cards">
+                <div className="cardItem">
+                    <img
+                        src={
+                            data["sprites"]["other"]["official-artwork"][
+                                "front_default"
+                            ]
+                        }
+                    />
+                    <div className="cardContent">
+                        <h3>{data?.name}</h3>
+                    </div>
+                    <ul className="list-group">
+                        <li className="list-group-item">
+                            Height:{data.height}
+                        </li>
+                        <li className="list-group-item">
+                            Weight:{data.weight}
+                        </li>
+                        <li className="list-group-item">
+                            List of abilities:
+                            {data.abilities.map((res, i) => {
+                                let commas = i === 0 ? "" : ", ";
+                                return commas + res.ability.name;
+                            })}
+                        </li>
+                    </ul>
+                    <div className="cardContent">
+                        <a href="#" className="btn">
+                            Button
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
     return (
         <>
             <div className="container">
                 <div className="cardLayout">
-                    <div className="cards">
-                        <div className="cardItem">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/24.png" />
-                            <div className="cardContent">
-                                <h3>arbok</h3>
-                            </div>
-                            <ul className="list-group">
-                                <li className="list-group-item">Height:35</li>
-                                <li className="list-group-item">Weight:650</li>
-                                <li className="list-group-item">
-                                    List of abilities:intimidate, shed-skin,
-                                    unnerve
-                                </li>
-                            </ul>
-                            <div className="cardContent">
-                                <button type="button" className="btn">
-                                    View Details
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                    {dataList?.map((item, i) => {
+                        return <Item data={item} key={i} />;
+                    })}
                 </div>
             </div>
         </>
